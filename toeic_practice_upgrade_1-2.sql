ccc1c2f556e0f8c2696df922818d5d2bd2d2aa5c
Beat me <th0jk3@gmail.com>
2017-10-18 22:28:33 +0700

    Add files via upload

---
+++ toeic_practice_upgrade_1-2.sql
@@ -0,0 +1,5 @@
+CREATE TABLE history (hid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, tid INT, time INT, score INT, access TEXT, has_tmp INT);
+CREATE TABLE test (tid INT PRIMARY KEY NOT NULL);
+INSERT INTO test VALUES (1),(2),(3),(4),(5),(6),(7),(8),(9),(10),(11),(12),(13),(14),(15),(16),(17),(18),(19),(20),(21),(22),(23),(24),(25),(26),(27),(28),(29),(30),(31),(32),(33),(34),(35),(36),(37),(38),(39),(40),(41),(42),(43),(44),(45),(46),(47),(48),(49),(50),(51),(52),(53),(54),(55),(56),(57),(58),(59),(60),(61),(62),(63),(64),(65),(66),(67),(68),(69),(70),(71),(72),(73),(74),(75),(76),(77),(78),(79),(80),(81),(82),(83),(84),(85);
+ALTER TABLE question ADD COLUMN ans_tmp INT;
+CREATE VIEW view_test_history AS SELECT test.tid, history.hid, history.time, history.score, history.access, history.has_tmp FROM test LEFT JOIN history ON test.tid = history.tid WHERE (hid IS NULL OR hid IN (SELECT max(hid) FROM history WHERE history.tid = test.tid)) ORDER BY test.tid ASC;
